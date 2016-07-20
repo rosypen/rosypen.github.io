8e57929fa8066b9959bea5c1514bf4af67c5a0e7
Demo User <demouser@MacBook-Air-5.local>
2016-07-19 17:24:15 -0700

    zoom update

--- profile.js
+++ profile.js
@@ -1,10 +1,10 @@
 $(document).ready(setup)
 function setup() {
   $("#Rosy").mouseenter(function(){
-    $(this).animate({height:"600px"}, 2000);
+    $(this).animate({height:"600px", width:"600px"}, 2000);
   });
   $("#Rosy").mouseleave(function(){
-    $(this).animate({height: "300px"}, 2000);
+    $(this).animate({height: "300px", width:"300px"}, 2000);
   });
   $("button").on("click" ,function(){
     //$("#thing").replaceWith(($("input").val()));
